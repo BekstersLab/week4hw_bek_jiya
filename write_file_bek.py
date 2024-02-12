a1cdb4bd1a156ef53a166580bb4b3d2e403eef1b
Bek <rebecca.e.rootes@gmail.com>
2024-02-12 16:53:10 +0000

    changed variable name from append to handle to prevent confusion

--- write_file_bek.py
+++ write_file_bek.py
@@ -1,16 +1,16 @@
 # Exercise - Write a line of code to create a file handle to open and append to a file called pelican.txt
-append = open('sample_files/pelican.txt', 'a')
+handle = open('sample_files/pelican.txt', 'a')
 # Exercise - append the following line to the file using the write method of the file handle:
-append.write("A wonderful bird is the pelican,\n")
+handle.write("A wonderful bird is the pelican,\n")
 
 # Exercise - Append the following second line using the write method:
-append.write("His bill holds more than his bellican.\n")
+handle.write("His bill holds more than his bellican.\n")
 
 # Exercise - Create a list that contains the following lines:
 lines = ["He can take in his beak, \n", "Enough food for a week,\n", "But I'm damned if I see how the helican.\n"]
 
 # Exercise - Append this list to the file using the writelines method.
-append.writelines(lines)
+handle.writelines(lines)
 # Why are the "\n" required?
 # writelines doesn't add line terminators to the end of each line, they need to be included in the list already
 
