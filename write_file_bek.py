25940d663c9141a821b5f06bc8192c7784a436a9
Bek <rebecca.e.rootes@gmail.com>
2024-02-12 16:27:31 +0000

    updated write_file

--- write_file_bek.py
+++ write_file_bek.py
@@ -1,17 +1,16 @@
 # Exercise - Write a line of code to create a file handle to open and append to a file called pelican.txt
-output = open('sample_files/pelican.txt', 'w')
-append = open('sample_files/logfile.txt', 'a')
+append = open('sample_files/pelican.txt', 'a')
 # Exercise - append the following line to the file using the write method of the file handle:
-output.write("A wonderful bird is the pelican,\n")
+append.write("A wonderful bird is the pelican,\n")
 
 # Exercise - Append the following second line using the write method:
-output.write("His bill holds more than his bellican.\n")
+append.write("His bill holds more than his bellican.\n")
 
 # Exercise - Create a list that contains the following lines:
 lines = ["He can take in his beak, \n", "Enough food for a week,\n", "But I'm damned if I see how the helican.\n"]
 
 # Exercise - Append this list to the file using the writelines method.
-output.writelines(lines)
+append.writelines(lines)
 # Why are the "\n" required?
 # writelines doesn't add line terminators to the end of each line, they need to be included in the list already
 
